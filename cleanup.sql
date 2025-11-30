543dd0297ba72c187fbf6145514d874dc516f9d3
Pelagia <marcinmalek00100@gmail.com>
2025-11-30 16:40:06 +0100

    cleanup code added

--- cleanup.sql
+++ cleanup.sql
@@ -1,3 +1,21 @@
+/*
+BEWARE!! THIS CODE REMOVES ALL TABLES
+*/
+DECLARE @sql NVARCHAR(MAX) = N'';
+
+-- Generate the ALTER TABLE...DROP CONSTRAINT statements
+SELECT @sql += N'ALTER TABLE ' 
+            + QUOTENAME(s.name) + N'.' + QUOTENAME(t.name) 
+            + N' DROP CONSTRAINT ' 
+            + QUOTENAME(fk.name) + N';' + CHAR(13)
+FROM sys.foreign_keys fk
+INNER JOIN sys.tables t ON fk.parent_object_id = t.object_id
+INNER JOIN sys.schemas s ON t.schema_id = s.schema_id;
+
+-- Execute the generated statements
+EXEC sp_executesql @sql;
+GO
+
 DECLARE @sql NVARCHAR(MAX) = N'';
 
 -- Generate DROP TABLE statements for all user tables
