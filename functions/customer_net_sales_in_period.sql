22b6700c5146b8fbe491c661b0ee68ae15891983
Jan Iłowski <kontakt@janilowski.pl>
2025-12-01 23:58:23 +0100

    feat: CustomerNetSalesInPeriod function
    Scalar function returning the net sales of a given customer in a given period

---
+++ functions/customer_net_sales_in_period.sql
@@ -0,0 +1,33 @@
+-- Customer Net Sales in Period
+-- Scalar function returning the net sales of a given customer in a given period
+--
+-- Usage:
+--SELECT
+--	dbo.fn_CustomerNetSalesInPeriod(1, '2023-01-01', '2023-12-31') AS NetSales2023;
+
+-- Helper:
+--SELECT c.ID cid, o.ID oid, o.CustomerID, (od.Quantity * od.UnitPrice), o.OrderDate FROM Customers c, Orders o, OrderDetails od WHERE c.ID = o.CustomerID AND o.ID = od.OrderID 
+
+
+CREATE FUNCTION dbo.fn_CustomerNetSalesInPeriod (
+	@CustomerID INT,
+	@FromDate	DATE,
+	@ToDate		DATE
+)
+RETURNS DECIMAL(18,2)
+AS
+BEGIN
+	DECLARE @NetValue DECIMAL(18,2);
+
+	SELECT
+		@NetValue = SUM(od.UnitPrice * od.Quantity * (1 - od.Discount))
+	FROM Orders o
+	JOIN OrderDetails od
+		ON od.OrderID = o.ID
+	WHERE o.CustomerID = @CustomerID
+		AND o.OrderDate >= @FromDate
+		AND o.OrderDate < DATEADD(DAY, 1, @ToDate);
+
+	RETURN ISNULL(@NetValue, 0);
+END;
+GO
