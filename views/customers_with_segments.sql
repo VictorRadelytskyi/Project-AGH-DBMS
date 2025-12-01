a191451211ff8208ec39147a6d48cd8355b57773
Jan Iłowski <kontakt@janilowski.pl>
2025-12-01 21:20:12 +0100

    feat: CustomersWithSegments view
    Proponuję bardziej modularną konstrukcję, może pozwoli ona na łatwiejsze
    orientowanie się w kodzie. Nie wiem do końca jaka jest tutaj najlepsza
    praktyk, także zrobiłem nowy folder na widoki i do każdego widoku będę
    tworzył oddzielny plik, który go tworzy.

---
+++ views/customers_with_segments.sql
@@ -0,0 +1,36 @@
+CREATE VIEW vw_CustomersWithSegments
+AS
+SELECT 
+	c.ID AS CustomerID,
+	c.ContactName,
+    c.ContactTitle,
+    c.Address,
+    c.City,
+    c.Region,
+    c.PostalCode,
+    c.Country,
+    c.PhoneNumber,
+    c.Fax,
+    cd.AgeGroup,
+    CASE cd.AgeGroup 
+    	WHEN 1 THEN '18-24'
+         WHEN 2 THEN '25-34'
+         WHEN 3 THEN '35-44'
+         WHEN 4 THEN '45-54'
+         WHEN 5 THEN '55-64'
+         WHEN 6 THEN '65+'
+         ELSE 'nieznana'
+	END AS AgeGroupLabel,
+	cd.IncomeGroup,
+	CASE cd.IncomeGroup 
+		WHEN 1 THEN 'niskie'
+		WHEN 2 THEN 'średnie'
+		WHEN 3 THEN 'wysokie'
+		ELSE 'nieznane'
+	END AS IncomeGroupLabel,
+	cd.HasChildren,
+	cd.IsCityResident
+FROM Customers c
+LEFT JOIN CustomerDemographics cd 
+	ON c.CustomerDemographicsID = cd.ID;
+GO
