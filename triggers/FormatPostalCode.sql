85d8f9d6daca2fe4439ebc0502e13beffe9b8335
Pelagia <marcinmalek00100@gmail.com>
2025-12-02 12:50:17 +0100

    one trigger added

---
+++ triggers/FormatPostalCode.sql
@@ -0,0 +1,21 @@
+CREATE TRIGGER FormatPostalCode ON Employees
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF TRIGGER_NESTLEVEL() > 1 RETURN;
+
+    IF EXISTS (
+        SELECT 1 
+        FROM inserted 
+        WHERE LEN(REPLACE(REPLACE(PostalCode, '-', ''), ' ', '')) < 5
+    )
+        THROW 51000, 'Kod pocztowy musi mieć co najmniej 5 znaków (po usunięciu spacji i myślników).', 1;
+
+    UPDATE e
+    SET PostalCode = REPLACE(REPLACE(i.PostalCode, '-', ''), ' ', '')
+    FROM Employees e
+    INNER JOIN inserted i ON e.ID = i.ID;
+END;
+GO
