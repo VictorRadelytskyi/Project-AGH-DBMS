4c363e27af4dac46a9fa1852a6d894a29e02a772
Pelagia <marcinmalek00100@gmail.com>
2025-12-02 09:59:38 +0100

    procedures decomposition added

---
+++ procedures/AddEmployee.sql
@@ -0,0 +1,58 @@
+-- Procedura AddEmployee - dodawanie nowego pracownika do bazy
+CREATE PROCEDURE AddEmployee @employeePositionID int,
+@firstName varchar(255),
+@lastName varchar(255),
+@birthDate date,
+@streetAddress varchar(2000),
+@city varchar(255),
+@region varchar(255),
+@postalCode varchar(16),
+@country varchar(255),
+@phoneNumber varchar(32),
+@ID INT OUTPUT 
+AS 
+BEGIN
+SET NOCOUNT ON;
+
+BEGIN TRY
+    BEGIN TRAN AddEmployee
+        INSERT INTO
+            Employees (
+                EmployeePositionID,
+                FirstName,
+                LastName,
+                BirthDate,
+                StreetAddress,
+                City,
+                Region,
+                PostalCode,
+                Country,
+                PhoneNumber
+            )
+        VALUES
+            (
+                @employeePositionID,
+                @firstName,
+                @lastName,
+                @birthDate,
+                @streetAddress,
+                @city,
+                @region,
+                @postalCode,
+                @country,
+                @phoneNumber
+            );
+
+        SET @ID = CAST(SCOPE_IDENTITY() AS INT);
+
+    COMMIT TRAN AddEmployee 
+END TRY 
+BEGIN CATCH 
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRAN AddEmployee 
+    DECLARE @msg NVARCHAR(2048) = 'Nie udało się dodać pracownika:' + CHAR(13) + CHAR(10) + ERROR_MESSAGE();
+    THROW 52000, @msg, 1;
+
+END CATCH
+END;
+GO
---
+++ procedures/RemoveComponent.sql
@@ -0,0 +1,30 @@
+-- Procedura RemoveComponent - usuwanie istniejącego komponentu z bazy
+CREATE PROCEDURE RemoveComponent @ID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRAN RemoveComponent
+
+        DELETE FROM Components
+        WHERE ID = @ID;
+
+        IF @@ROWCOUNT = 0
+            THROW 51000, 'Nie znaleziono półproduktu o podanym ID.', 1;
+
+        COMMIT TRAN RemoveComponent
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRAN RemoveComponent
+        
+        DECLARE @msg NVARCHAR(2048) = 'Nie udało się usunąć półproduktu:' + CHAR(13) + CHAR(10) + ERROR_MESSAGE();
+
+        IF ERROR_NUMBER() = 547
+            SET @msg = 'Nie można usunąć komponentu, ponieważ jest on używany w innych tabelach.' + CHAR(13) + CHAR(10) + ERROR_MESSAGE();
+
+        THROW 52000, @msg, 1;
+    END CATCH
+END;
+GO
