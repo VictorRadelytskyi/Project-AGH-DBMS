9fa13f07bab6e5bfda0932da432e26d4d217bde8
janilowski <kontakt@janilowski.pl>
2025-12-08 20:09:38 +0100

    fix: OrderNetValue should use the VAT Multipler

--- functions/order_net_value.sql
+++ functions/order_net_value.sql
@@ -1,21 +1,28 @@
 -- Order Net Value
 -- Scalar function returning the net value of a given order
+-- Uses Products.VATMultipler and net UnitPrice from OrderDetails
 --
 -- Usage:
 --SELECT dbo.fn_OrderNetValue(1) AS OrderNetValue;
 -- (using `dbo.` to make sure you're executing the function in the correct DB)
 
-CREATE FUNCTION fn_OrderNetValue (@OrderID INT)
+CREATE FUNCTION dbo.fn_OrderNetValue (@OrderID INT)
 RETURNS DECIMAL(18,2)
 AS
 BEGIN
-	DECLARE @NetValue DECIMAL(18,2);
+    DECLARE @NetValue DECIMAL(18,2);
 
-	SELECT
-		@NetValue = SUM(od.UnitPrice * od.Quantity * (1 - od.Discount))
-	FROM OrderDetails od
-	WHERE od.OrderID = @OrderID;
+    SELECT
+        @NetValue = SUM(
+              od.UnitPrice          -- net unit price
+            * od.Quantity
+            * (1 - od.Discount)
+            * p.VATMultipler        -- apply VAT for each product
+        )
+    FROM OrderDetails od
+    JOIN Products     p ON p.ID = od.ProductID
+    WHERE od.OrderID = @OrderID;
 
-	RETURN ISNULL(@NetValue, 0);
+    RETURN ISNULL(@NetValue, 0);
 END;
 GO
