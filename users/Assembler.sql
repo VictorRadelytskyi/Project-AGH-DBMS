29506d5e364dac96ca5febfc8118cafdda1f4e2c
Jan Iłowski <kontakt@janilowski.pl>
2025-12-21 14:29:30 +0100

    feat: assembler employee role

---
+++ users/Assembler.sql
@@ -0,0 +1,25 @@
+/*
+Production floor staff who actually manufacture the products.
+*/
+CREATE ROLE Role_Assembler;
+GO
+
+GRANT SELECT ON dbo.Orders TO Role_Assembler;
+GRANT SELECT ON dbo.OrderDetails TO Role_Assembler;
+GRANT SELECT ON dbo.Customers TO Role_Assembler;
+
+GRANT EXECUTE ON dbo.sp_StartOrderFulfillment TO Role_Assembler;
+GRANT EXECUTE ON dbo.sp_UpdateOrderItemProgress TO Role_Assembler;
+GRANT EXECUTE ON dbo.sp_CompleteOrderFulfillment TO Role_Assembler;
+GRANT EXECUTE ON dbo.sp_UpdateOrderItemProgress TO Role_Assembler;
+GRANT EXECUTE ON dbo.sp_IncrementOrderItemProgress TO Role_Assembler;
+
+GRANT EXECUTE ON dbo.ConsumeComponentInventory TO Role_Assembler;
+
+GRANT EXECUTE ON dbo.AddProductRecipe.sql TO Role_Assembler;
+GRANT EXECUTE ON dbo.AddProductToWarehouse TO Role_Assembler;
+GRANT EXECUTE ON dbo.AddRecipeIngredient TO Role_Assembler;
+GRANT EXECUTE ON dbo.UpdateComponent TO Role_Assembler;
+GRANT EXECUTE ON dbo.UpdateProductRecipe TO Role_Assembler;
+
+GO
