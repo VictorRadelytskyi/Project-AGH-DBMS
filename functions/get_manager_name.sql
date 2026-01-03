bcf1aec808fdf983e3f703993b87dc4ab9946f48
Pelagia <marcinmalek00100@gmail.com>
2026-01-03 21:18:07 +0100

    functions improvements

--- functions/get_manager_name.sql
+++ functions/get_manager_name.sql
@@ -2,27 +2,18 @@ CREATE FUNCTION dbo.fn_GetManagerName (@EmployeeID INT)
 RETURNS VARCHAR(512)
 AS
 BEGIN
-    BEGIN TRY
 
-        DECLARE @ManagerName VARCHAR(512);
-        DECLARE @ManagerID INT;
+    DECLARE @ManagerName VARCHAR(512);
+    DECLARE @ManagerID INT;
 
-        SELECT @ManagerID = ReportsTo
-        FROM Employees
-        WHERE ID = @EmployeeID;
+    SELECT @ManagerID = ReportsTo
+    FROM Employees
+    WHERE ID = @EmployeeID;
 
-        IF @ManagerID IS NOT NULL
-            SET @ManagerName = dbo.fn_GetEmployeeFullName(@ManagerID);
+    IF @ManagerID IS NOT NULL
+        SET @ManagerName = dbo.fn_GetEmployeeFullName(@ManagerID);
 
-    END TRY
-    BEGIN CATCH
+    RETURN ISNULL(@ManagerName, 'Brak przełożonego');
 
-        DECLARE @msg NVARCHAR(2000);
-        SET @msg = FORMATMESSAGE("Problem z pobraniem przełożonego pracownika o ID %d", @EmployeeID) + CHAR(13) + CHAR(10) + ERROR_MESSAGE();
-        THROW 52000, @msg, 1;
-    
-    END CATCH
-
-    RETURN ISNULL(@ManagerName, "Brak przełożonego");
 END;
 GO
