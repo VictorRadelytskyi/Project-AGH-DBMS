40525688423e3aaacd01d0419ea5e34a7e2dfb94
Pelagia <marcinmalek00100@gmail.com>
2026-01-03 21:18:07 +0100

    functions improvements

--- functions/get_employee_full_name.sql
+++ functions/get_employee_full_name.sql
@@ -2,23 +2,14 @@ CREATE FUNCTION dbo.fn_GetEmployeeFullName (@EmployeeID INT)
 RETURNS VARCHAR(512)
 AS
 BEGIN
-    BEGIN TRY
 
-        DECLARE @FullName VARCHAR(512);
+    DECLARE @FullName VARCHAR(512);
 
-        SELECT @FullName = e.FirstName + ' ' + e.LastName
-        FROM Employees e
-        WHERE e.ID = @EmployeeID;
-
-    END TRY
-    BEGIN CATCH
-
-        DECLARE @msg NVARCHAR(2000);
-        SET @msg = FORMATMESSAGE("Problem z pobraniem nazwy pracownika o ID %d", @EmployeeID) + CHAR(13) + CHAR(10) + ERROR_MESSAGE();
-        THROW 52000, @msg, 1;
-
-    END CATCH
+    SELECT @FullName = e.FirstName + ' ' + e.LastName
+    FROM Employees e
+    WHERE e.ID = @EmployeeID;
 
     RETURN @FullName;
+    
 END;
 GO
